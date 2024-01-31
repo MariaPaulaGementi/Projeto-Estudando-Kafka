3d0bfedaf13c8d94f006abfb0b0c91f1e1b5f806
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2024-01-31 13:20:41 -0300

    Implementando a função Callback no senMenssage

--- src/main/java/com/valdir/strproducer/service/StringProducerService.java
+++ src/main/java/com/valdir/strproducer/service/StringProducerService.java
@@ -1,10 +1,12 @@
 package com.valdir.strproducer.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -17,6 +19,14 @@ public class StringProducerService {
     }
 
     public void sendMenssage(String message){
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if (success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {}, Offset {}", success.getRecordMetadata().partition(), success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error sen message")
+        );
     }
 }
