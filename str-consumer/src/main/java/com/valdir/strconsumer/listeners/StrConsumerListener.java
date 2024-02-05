9f784d32b438a3eed01be0cc61388ffca73c7f75
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2024-02-05 14:53:51 -0300

    Implementando Listener Customizado

--- str-consumer/src/main/java/com/valdir/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/valdir/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.valdir.strconsumer.listeners;
 
+import com.valdir.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -8,19 +9,17 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class StrConsumerListener {
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic ="srt-topic", partitions = {"0"} )
-            }, containerFactory = "strContainerFactory")
+    //forma de criar particao sem usar o listener customizado
+//    @KafkaListener(groupId = "group-1",
+//            topicPartitions = {
+//                    @TopicPartition(topic ="srt-topic", partitions = {"0"} )
+//            }, containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
       //  log.info("Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-            @TopicPartition(topic ="srt-topic", partitions = {"1"} )
-            },
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         //  log.info("Receive message {}", message);
     }
