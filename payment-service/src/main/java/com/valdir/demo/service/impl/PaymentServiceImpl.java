b0ca0c36a22758e013f90d94e94243e1db6e10e2
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2024-02-19 11:57:57 -0300

    Configurando produtor para objetos Json

--- payment-service/src/main/java/com/valdir/demo/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/valdir/demo/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,34 @@ package com.valdir.demo.service.impl;
 
 import com.valdir.demo.model.Payment;
 import com.valdir.demo.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private KafkaTemplate<String, Serializable> kafkaTemplate;
+
     Logger log = LoggerFactory.getLogger(PaymentServiceImpl.class);
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
         log.info("Recebi o pagamento {}", payment);
+       // Thread.sleep(1000);
+
+        log.info("Pagamento Enviado");
+       kafkaTemplate.send("payment-topic",  payment);
+
+
     }
+
 }
